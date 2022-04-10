fix(retriever) add scaling for dot product, add(main) add task selection for specific training

main_test.py
@@ -2,13 +2,13 @@ import torch
 from torchdrug import data, datasets
 from torchdrug import core, models, tasks, utils
 from torch.nn import functional as F
+import GINE
 import time
 import pickle
 from pathlib import Path
 import os
-import numpy as np
-import GINE
 from retrieval_task import RetrievalReader
+import numpy as np
 from retriever import Retriever
 from encoder_retriever import encoder_retriever
 
@@ -22,33 +22,27 @@ def dataset_download(path):
     return dataset, train_set, valid_set, test_set
 
 # encoder pretraining
-def encoder_pretraining(dataset, task, train_set, valid_set, test_set, num_evidence, evidence_dim, hidden_dims, encoder_path=None, load=False, load_path=None):
+def encoder_pretraining(dataset, train_set, valid_set, test_set, num_evidence, evidence_dim, hidden_dims, encoder_path=None, load=False, load_path=None):
     ef_dim = valid_set[0].pop('graph').edge_feature.shape[1]
 
-    '''    
     encoder = models.GIN(input_dim=dataset.node_feature_dim,
-                       hidden_dims=[256, 256, 256, 256],
-                       short_cut=True, batch_norm=True, concat_hidden=True)
-    '''
-    encoder = GINE.GINE(num_evidence= num_evidence, evidence_dim=evidence_dim, input_dim=dataset.node_feature_dim,
-                       hidden_dims=hidden_dims, edge_input_dim=ef_dim,
-                       short_cut=True, batch_norm=True, concat_hidden=True)
-    print(task)
-    encoder_task = tasks.PropertyPrediction(encoder, task=task,
+                         hidden_dims=[256, 256, 256, 256],
+                         short_cut=True, batch_norm=True, concat_hidden=True)
+    task = tasks.PropertyPrediction(encoder, task=dataset.tasks,
                                 criterion="bce", metric=("auprc", "auroc"))
 
-    optimizer = torch.optim.Adam(encoder_task.parameters(), lr=1e-3)
-    encoder_solver = core.Engine(encoder_task, train_set, valid_set, test_set, optimizer, gpus=[0], batch_size=1024)
+    optimizer = torch.optim.Adam(task.parameters(), lr=1e-3)
+    encoder_solver = core.Engine(task, train_set, valid_set, test_set, optimizer, gpus=[0], batch_size=1024)
     if load:
         assert load_path is not None
         encoder_solver.load(load_path)
-        encoder_solver.evaluate("valid")
     else:
         assert encoder_path is not None
         encoder_solver.train(num_epoch=100)
         encoder_solver.evaluate("valid")
         tm = '_'.join(time.ctime().split())
         encoder_solver.save(encoder_path + tm + '_encoder.pt')
+        encoder_solver.evaluate("valid")
     return encoder_solver, encoder
 
 # number the molecules in train_set
@@ -61,13 +55,14 @@ def evidence_enumeration(dataset, train_set):
     return dataset
 
 # retrieve evidence label
-def evidence_label(dataset, task, train_set, device):
-    assert 'EMB' not in task
+def evidence_label(dataset, train_set, device):
+    train_tasks = dataset.tasks
     train_ind = np.array(train_set.indices)
     targets = dataset.targets
-    num_types_labels = len(task)
+    num_labels = len(train_tasks) - 1
     emb_label = []
-    for label in task:
+    for i in range(num_labels):
+        label = train_tasks[i]
         full_label = np.array(targets[label])
         evidence_label = full_label[train_ind]
         emb_label.append(evidence_label.tolist())
@@ -94,12 +89,14 @@ def pretrain_index(encoder, train_set, evidence_dim, evidence_dir):
     return emb_file
 
 # retriever model
-def retriever_model(dataset, task, train_set, valid_set, test_set, encoder, topk, emb_label, num_class, emb_file, retriever_path):
-    retrieval = Retriever(dim=1024, emb_path=emb_file, emb_label=emb_label, num_label_types=num_class, topk=topk, num_evidence=len(train_set), out_fation='sum')
+def retriever_model(dataset, train_set, valid_set, test_set, encoder, emb_label, emb_file, retriever_path):
+    assert emb_label[emb_label > 1].sum() + emb_label[emb_label < 0].sum() == 0
+    retrieval = Retriever(dim=1024, emb_path=emb_file, emb_label=emb_label, topk=10, num_evidence=len(train_set), out_fation='sum')
     enc_retr = encoder_retriever(encoder=encoder, retriever=retrieval)
-    assert 'EMB' not in task
-    retrieval_task = RetrievalReader(model=enc_retr, task=task,
-                                    criterion="bce", metric=("auprc", "auroc"), num_class=num_class)
+    training_task = dataset.tasks[:-1]
+    assert 'EMB' not in training_task
+    retrieval_task = RetrievalReader(model=enc_retr, task=training_task,
+                                    criterion="bce", metric=("auprc", "auroc"), num_class=2)
 
     retrieval_optimizer = torch.optim.Adam(retrieval_task.parameters(), lr=1e-3)
     retrieve_solver = core.Engine(retrieval_task, train_set, valid_set, test_set, retrieval_optimizer, gpus=[0], batch_size=1024)
@@ -121,18 +118,14 @@ def main():
 
     hidden_dims = [256, 256, 256, 256]
     evidence_dim = sum(hidden_dims)
-    task = [dataset.tasks[1]].copy()
-    num_class = 1
-    print(task)
-    encoder_solver, encoder = encoder_pretraining(dataset, task, train_set, valid_set, test_set, num_evidence, evidence_dim, hidden_dims,
-                                                  encoder_path=encoder_path, load=False, load_path=load_path)
+
+    encoder_solver, encoder = encoder_pretraining(dataset, train_set, valid_set, test_set, num_evidence, evidence_dim, hidden_dims,
+                                                  encoder_path=encoder_path, load=False, load_path=None)
     device = encoder_solver.device
     dataset = evidence_enumeration(dataset, train_set)
-    print(task)
-    emb_label = evidence_label(dataset, task, train_set, device)
-    emb_file = pretrain_index(encoder, train_set, evidence_dim, evidence_dir='/content/emb/')
-    print(task)
-    retriever_solver, retrieval = retriever_model(dataset, task, train_set, valid_set, test_set, encoder, 10, emb_label, num_class, emb_file, retriever_path)
+    emb_label = evidence_label(dataset, train_set, device)
+    emb_file = pretrain_index(encoder, train_set, evidence_dim, evidence_dir='/content/emb')
+    retriever_solver, retrieval = retriever_model(dataset, train_set, valid_set, test_set, encoder, emb_label, emb_file, retriever_path)
 
 if __name__ == '__main__':
     main()

retriever2.py
@@ -61,7 +61,8 @@ class Retriever(nn.Module, core.Configurable):
             kvalue = kvalue[:, 1:]
             kind = kind[:, 1:]
             kself = kind[:, 0]
-            kself = kself.unsqueeze(-1)
+            #kself = kself.unsqueeze(-1)
+            '''
             for i in range(self.emb_label.shape[0]):
                 label_self = torch.gather(self.emb_label[i].expand(batch_size, self.num_evidence), -1, kself).cuda()
                 klabel = torch.gather(self.emb_label[i].expand(batch_size, self.num_evidence), -1, kind).cuda()
@@ -71,24 +72,14 @@ class Retriever(nn.Module, core.Configurable):
                 b_s = len(hit_rate)
                 rate = hit_rate.sum() / b_s
                 print(rate)
+            '''
         elif mode == 'test':
             kvalue, kind = torch.topk(score, self.k, dim=-1)
         else:
             raise NotImplementedError
         kvalue /= torch.sqrt(kvalue)
         kvalue = self.sfm(kvalue)
-        # TODO: whether or not to add representations of different labels together? Or concatenate?
-        res = []
-        for i in range(self.emb_label.shape[0]):
-            klabel = torch.gather(self.emb_label[i].expand(batch_size, self.num_evidence), -1, kind).cuda()
-            klabel_emb = self.label_embedding[i](klabel)
-            weighted_label_emb = klabel_emb * kvalue.unsqueeze(-1)
-            res_emb = weighted_label_emb.sum(dim=1)
-            res.append(res_emb)
-        if self.out_fation == 'sum':
-            return sum(res)
-        elif self.out_fation == 'concat':
-            return torch.cat(res, dim=1)
-        else:
-            raise NotImplementedError
+        sum_emb = self.evidence_emb(kind) * kvalue.unsqueeze(-1)
+        return sum_emb.sum(1)
+        # direct sum of top k graph representation
 

test3.py
@@ -7,6 +7,7 @@ import GINE
 from GINE import VN
 import numpy as np
 from torchdrug import tasks
+from torchdrug.layers import functional
 
 
 mol = data.Molecule.from_smiles("C1=CC=CC=C1", node_feature="default",
@@ -19,8 +20,64 @@ lengths += [len(dataset) - sum(lengths)]
 train_set, valid_set, test_set = torch.utils.data.random_split(dataset, lengths)
 sample = dataset[0]
 sample2 = dataset[1]
+sample3 = dataset[2]
+
+g1 = sample['graph']
+g2 = sample2['graph']
+g3 = sample3['graph']
+
+print(g1)
+print(g1.to_smiles())
+g1.visualize(save_file='/Users/zhaoshiyu/PycharmProjects/retrieval/test.png')
+print(g1.atom_type)
+print(g1.bond_type)
+print(g1.node_feature.shape)
+va = VN()
+new_s = va(g1)
+print(new_s)
+print(new_s.atom_type)
+print(new_s.bond_type)
+print(new_s.node_feature.shape)
+
+sample = dataset[0]
+sample2 = dataset[1]
+sample3 = dataset[2]
+
+g1 = sample['graph']
+g2 = sample2['graph']
+g3 = sample3['graph']
+
+pg = data.Graph.pack([g1, g2, g3])
+print(pg)
+print(pg.batch_size)
+print(pg.num_nodes)
+print(pg.num_relation)
+print(pg.atom_type)
+print(pg.node_feature.shape)
+print(pg.edge_list.shape)
+
+def _append(data, num_xs, input, mask=None):
+    if mask is None:
+        mask = torch.ones_like(num_xs, dtype=torch.bool)
+    new_num_xs = num_xs + mask
+    new_num_cum_xs = new_num_xs.cumsum(0)
+    print('new_num_cum_xs', new_num_cum_xs)
+    new_num_x = new_num_cum_xs[-1].item()
+    new_data = torch.zeros(new_num_x, *data.shape[1:], dtype=data.dtype, device=data.device)
+    print('new_data', new_data)
+    starts = new_num_cum_xs - new_num_xs
+    ends = starts + num_xs
+    print('starts', starts)
+    print('ends', ends)
+    index = functional.multi_slice_mask(starts, ends, new_num_x)
+    print('index', index)
+    new_data[index] = data
+    new_data[~index] = input[mask]
+    return new_data, new_num_xs
 
 
+
+'''
 print(dataset.targets)
 def a():
     train_ind = np.array(train_set.indices)
@@ -33,7 +90,6 @@ print(train_set[0])
 print(train_set[1])
 print(train_set[2])
 
-'''
 tasks = dataset.tasks
 targets = dataset.targets
 num_labels = len(tasks) - 1